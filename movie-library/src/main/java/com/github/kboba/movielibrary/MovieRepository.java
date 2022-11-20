8f7b3f13c6552daeb2494ff448501ddbcf798fc5
BobaK <boba.krzysztof98@gmail.com>
2022-11-20 16:56:19 +0100

    Read all movies

--- movie-library/src/main/java/com/github/kboba/movielibrary/MovieRepository.java
+++ movie-library/src/main/java/com/github/kboba/movielibrary/MovieRepository.java
@@ -1,13 +1,21 @@
 package com.github.kboba.movielibrary;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class MovieRepository {
 
     @Autowired
     JdbcTemplate jdbcTemplate;
+
+    public List<Movie> getAll(){
+        return jdbcTemplate.query("SELECT id, title, director, rating FROM movie",
+                BeanPropertyRowMapper.newInstance(Movie.class));
+    }
 
 }
